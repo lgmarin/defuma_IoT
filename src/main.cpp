eee025576523adbd06341f5ba4037333fdbbdac0
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-06-02 19:39:57 -0300

    add dependencies

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <SPI.h>
 #include <TM1637Display.h>
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 
 // USING GPIO PINS FOR ESP12 Compatibility!
 // D# Pins correspond to nodeMCU V1.2 pins
@@ -26,72 +29,72 @@ float temp_low = 24.00;
 float temp_high = 26.00;
 
 double readTemperatureC(uint8_t CS) {
-    //READ MAX6675 Temperature in Celsius using SPI Interface
-    uint16_t v;
+  //READ MAX6675 Temperature in Celsius using SPI Interface
+  uint16_t v;
 
-    digitalWrite(CS, LOW);
-    v = SPI.transfer(0x00);
-    v <<= 8;
-    v |= SPI.transfer(0x00);
-    digitalWrite(CS, HIGH);
+  digitalWrite(CS, LOW);
+  v = SPI.transfer(0x00);
+  v <<= 8;
+  v |= SPI.transfer(0x00);
+  digitalWrite(CS, HIGH);
 
-    if (v & 0x4) {
-        // No thermocouple attached, return 0
-        return NAN; 
-    }
+  if (v & 0x4) {
+      // No thermocouple attached, return 0
+      return NAN; 
+  }
 
-    v >>= 3;
+  v >>= 3;
 
-    return v*0.25;
+  return v*0.25;
 }
 
 void setup(){
-    //Intiate SPI transaction
-    SPI.begin();
+  //Intiate SPI transaction
+  SPI.begin();
 
-    //Set max_CS Pin as OUTPUT and set to HIGH to Intiate MAX 6675
-    pinMode(max_CS, OUTPUT);
-    digitalWrite(max_CS, HIGH);
+  //Set max_CS Pin as OUTPUT and set to HIGH to Intiate MAX 6675
+  pinMode(max_CS, OUTPUT);
+  digitalWrite(max_CS, HIGH);
 
-    // Set Buzzer PIN as Output
-    pinMode(BUZZ, OUTPUT);
+  // Set Buzzer PIN as Output
+  pinMode(BUZZ, OUTPUT);
 
-    // Prepare Display and Wait for MAX initialization
-    uint8_t data[] = { 0xff, 0xff, 0xff, 0xff };
-    display.setBrightness(0x0f);
-    display.setSegments(data);
+  // Prepare Display and Wait for MAX initialization
+  uint8_t data[] = { 0xff, 0xff, 0xff, 0xff };
+  display.setBrightness(0x0f);
+  display.setSegments(data);
 
-    delay(500);
+  delay(500);
 
-    Serial.begin(9600);
-    display.clear();
+  Serial.begin(9600);
+  display.clear();
 }
 
 void loop(){
-    //Read Temperature Data
-    temp = readTemperatureC(max_CS);
+  //Read Temperature Data
+  temp = readTemperatureC(max_CS);
 
-    if (temp == NAN)
-    {
-      Serial.print("Error: No Thermocouple connected!");
-    } 
-    else
-    {
-      Serial.print("Temperature: ");
-      Serial.print(temp);
-      Serial.print(" C");
-      display.showNumberDec(temp);
-    }
-    Serial.print("\n");
+  if (temp == NAN)
+  {
+    Serial.print("Error: No Thermocouple connected!");
+  } 
+  else
+  {
+    Serial.print("Temperature: ");
+    Serial.print(temp);
+    Serial.print(" C");
+    display.showNumberDec(temp);
+  }
+  Serial.print("\n");
 
-    if (temp != NAN && temp > temp_high) {
-      tone(BUZZ, 523, 800);
-    } else if (temp != NAN && temp < temp_low) {
-      tone(BUZZ, 1000, 200);
-    } else {
-      noTone(BUZZ);
-    }
+  if (temp != NAN && temp > temp_high) {
+    tone(BUZZ, 523, 800);
+  } else if (temp != NAN && temp < temp_low) {
+    tone(BUZZ, 1000, 200);
+  } else {
+    noTone(BUZZ);
+  }
 
-    delay(1000);
-    //noTone(BUZZ);
+  delay(1000);
+  //noTone(BUZZ);
 }
