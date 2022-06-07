1eed8fcf29283f508ea442bb1d52c249437f41e6
lgmarin <11378945+lgmarin@users.noreply.github.com>
2022-06-07 13:56:45 -0300

    Refactoring - indexhtml.h

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+#include <index_html.h>
 
 // USING GPIO PINS FOR ESP12 Compatibility!
 // D# Pins correspond to nodeMCU V1.2 pins
@@ -32,30 +33,6 @@ const char* password = "Your_Password";    //replace with your password
 String temp_low = "24.0";
 String temp_high = "26.0";
 String last_temperature;
-
-// Web Page Code to be stored in Memory
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html><head>
-  <title>ESP Thermostat Web server</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <style>
-    html {font-family: Times New Roman; display: inline-block; text-align: center;}
-    h2 {font-size: 3.0rem;}
-    h3 {font-size: 2.0rem; color: #FF0000;}
-  </style>
-  
-  </head><body>
-  <h2>Defumadeira Controle Remoto</h2> 
-  <h3>%TEMPERATURE% &deg;C</h3>
-  <h2>Níveis de Temperatura</h2>
-  <form action="/get">
-    Temperatura Máxima <input type="number" step="0.1" name="threshold_max" value="%THRESHOLD_MAX%" required><br>
-    Temperatura Mínima <input type="number" step="0.1" name="threshold_min" value="%THRESHOLD_MIN%" required><br>
-    <br/>
-    <input type="submit" value="Enviar">
-  </form>
-</body></html>)rawliteral";
 
 void notFound(AsyncWebServerRequest *request) {
   request->send(404, "text/plain", "Not found");
